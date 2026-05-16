9132831f6caacd0c9d726f2b308b3037b9b8624d
midnight-sway-atomic-admin <mikejellingsworth@gmail.com>
2026-05-16 04:37:51 +0000

    Update toggle-sunset.sh

--- .config/rofi/scripts/toggle-sunset.sh
+++ .config/rofi/scripts/toggle-sunset.sh
@@ -1,9 +1,11 @@
-/* Bluelight-filter toggle */
+# Bluelight-filter toggle
+
+# First make it executable with chmod +x "$HOME/.config/rofi/scripts/toggle-sunset.sh"
 
 #!/bin/sh
 
 if pgrep -x wlsunset >/dev/null; then
   pkill -x wlsunset
 else
-  wlsunset -T 1500 -t 1000 &
+  nohup wlsunset -T 1500 -t 1000 >/dev/null 2>&1 &
 fi
