c1ba79f54232b160cb19b84fb768042dcbc3ef5f
midnight-sway-atomic-admin <mikejellingsworth@gmail.com>
2026-06-17 04:24:22 +0000

    Add files via upload

---
+++ Firefox/user.js
@@ -0,0 +1,11 @@
+/* Install this file in the same place that you have installed the "chrome" folder */
+
+/* X Suppresses non-tab Firefox tooltips */
+user_pref("browser.chrome.toolbar_tips", false);
+
+/* X Keeps tab hover tooltip/card */
+user_pref("browser.tabs.hoverPreview.enabled", true);
+user_pref("browser.tabs.hoverPreview.showThumbnails", false);
+
+/* Suppresses "This time search with..." URL-button */
+user_pref("browser.urlbar.scotchBonnet.enableOverride", false);
