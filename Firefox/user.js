4ae2705564eb1879f4ead44255aea60ebd009903
midnight-sway-atomic-admin <mikejellingsworth@gmail.com>
2026-06-28 04:06:17 +0000

    Update user.js

--- Firefox/user.js
+++ Firefox/user.js
@@ -1,29 +1,29 @@
 /* Install this file in the same place that you have installed the "chrome" folder */
 
-/* X Suppresses non-tab Firefox tooltips */
+/* Suppresses non-tab Firefox tooltips */
 user_pref("browser.chrome.toolbar_tips", false);
 
-/* X Keeps tab hover tooltip/card */
+/* Keeps tab hover tooltip/card */
 user_pref("browser.tabs.hoverPreview.enabled", true);
 user_pref("browser.tabs.hoverPreview.showThumbnails", false);
 
-/* X Suppresses "This time search with..." URL-button */
+/* Suppresses "This time search with..." URL-button */
 user_pref("browser.urlbar.scotchBonnet.enableOverride", false);
 
-/* X Ensures the "Add Bookmark" popup is always shown after pressing Ctrl+D; the "Add Bookmark" popup no longer contains the text or check box for enabling the editor for every new bookmark */
+/* Ensures the "Add Bookmark" popup is always shown after pressing Ctrl+D; the "Add Bookmark" popup no longer contains the text or check box for enabling the editor for every new bookmark */
 user_pref("browser.bookmarks.editDialog.showForNewBookmarks", true);
 
-/* X Enables CSS customization via userChrome.css and userContent.css */
+/* Enables CSS customization via userChrome.css and userContent.css */
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
-/* X Prevents the full-screening of videos by tapping the touchpad or otherwise; on YouTube use Mod+F to enlarge the videoplayer */
+/* Prevents the full-screening of videos by tapping the touchpad or otherwise; on YouTube use Mod+F to enlarge the videoplayer */
 user_pref("full-screen-api.enabled", false);
 
-/* X Warns before closing Firefox */
+/* Warns before closing Firefox */
 user_pref("browser.tabs.warnOnClose", true);
 
-/* Enables the ability to set Density to Compact via Customize Toolbar */
-user_pref("browser.compactmode.show", true);
+/* Hides tab site icons */
+user_pref("browser.chrome.site_icons", false);
 
-/* Controls the size of Firefox's UI; for a 2560x1600 display with scaling set to 2, "1.6" is a good value; the default is "-1.0" 
-user_pref("layout.css.devPixelsPerPx", 1.6); */
+/* Changes the size of Firefox's UI; for a 2560x1600 display with scaling set to 2, "1.6" is a good value; the default is "-1.0" */ 
+/* user_pref("layout.css.devPixelsPerPx", 1.6); */
